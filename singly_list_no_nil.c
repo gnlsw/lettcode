9ce5c1ecc169c8b4f5fa2069b2cc1241ed587c72
gnlsw <today_wsl@163.com>
2016-05-31 07:46:40 +0800

    单链表，无头节点，暂时为空函数

---
+++ singly_list_no_nil.c
@@ -0,0 +1,33 @@
+/***********************************
+Function : 单链表，在头节点处无哨兵
+Date     : 2016.05.31
+Author   : gnlsw
+***********************************/
+#include <stdio.h>
+#include <stdlib.h>
+
+#define VOS_OK         0
+#define VOS_ERR        1
+#define VOS_NULL_PTR   0
+
+struct ListNode {
+    int val;
+    struct ListNode *next;
+}
+
+int main()
+{
+    return VOS_OK;
+}
+
+int addListNodeToHead(struct ListNode *pHead)
+{
+}
+
+int addListNodeToTail(struct ListNode *pHead)
+{
+}
+
+struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2)
+{
+}
