95e288ca781d7466725b0c84e23eb61c39878e88
gnlsw <today_wsl@163.com>
2017-04-16 18:13:52 +0800

    支持开头的空格和中间的字母

--- prob_8.c
+++ prob_8.c
@@ -1,11 +1,12 @@
 #include "stdio.h"
 #include "string.h"
+#include "ctype.h"
 
 int myAtoi(char *str);
 
 int main()
 {
-    char * auc_var = "1234";
+    char * auc_var = "  12ac34";
     int    int_var = myAtoi(auc_var);
 
     printf("%d\n", int_var);
@@ -15,9 +16,15 @@ int main()
 
 int myAtoi(char *str)
 {
-    int sum = 0;
-    int loop;
+    int sum  = 0;
+    int loop = 0;
     int int_strlen = strlen(str);
+    int in_dig = 0;
+
+    if(NULL == str)
+    {
+        return 0;
+    }
 
     /* input null, return 0 */
     if(0 == int_strlen)
@@ -26,17 +33,40 @@ int myAtoi(char *str)
     }
 
     /* check illegal char */
-    for(loop = 0; loop < int_strlen; loop++)
+    while('\0' != str[loop])
     {
-        if(str[loop] < '0' || str[loop] > '9')
+        /* not dig yet */
+        if(in_dig == 0)
         {
-            return 0;
+            if(isspace(str[loop]))
+            {
+                loop++;
+                continue;
+            }
+            else if(isdigit(str[loop]))
+            {
+                in_dig = 1;
+                sum = str[loop] - '0';
+                loop++;
+            }
+            else
+            {
+                break;
+            }
         }
-    }
+        else
+        {
 
-    for(loop = 0; loop < int_strlen; loop++)
-    {
-        sum = sum * 10 + (str[loop] - '0');
+            if(isdigit(str[loop]))
+            {
+                sum = sum * 10 + (str[loop] - '0');
+                loop++;
+            }
+            else
+            {
+                break;
+            }
+        }
     }
 
     return sum;
